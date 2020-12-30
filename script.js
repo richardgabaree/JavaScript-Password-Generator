2c0d0c04933618fa8d2fc818d7ed03945ede8c6d
Richard Gabaree <richard.gabaree@gmail.com>
2020-12-30 17:55:44 -0600

    added variables to represent all available characters

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var symbols = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~ ";
+
