fe01414201f94430adfd49c93523f13d23b980a8
Richard Gabaree <richard.gabaree@gmail.com>
2020-12-30 18:01:09 -0600

    added funtion to generate password

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
 var symbols = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~ ";
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
